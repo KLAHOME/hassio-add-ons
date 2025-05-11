c8204c0f1710381e78e09c239a51277cc2197f8a
Kevin Latka <k.latka@outlook.de>
2025-05-11 11:26:22 +0200

    Add log level to newt add-on

--- newt/run.sh
+++ newt/run.sh
@@ -13,6 +13,10 @@ fi
 PANGOLIN_ENDPOINT=$(jq -r '.PANGOLIN_ENDPOINT' "$CONFIG_PATH")
 NEWT_ID=$(jq -r '.NEWT_ID' "$CONFIG_PATH")
 NEWT_SECRET=$(jq -r '.NEWT_SECRET' "$CONFIG_PATH")
+LOG_LEVEL=$(jq -r '.LOG_LEVEL' "$CONFIG_PATH")
+if [[ "$LOG_LEVEL" == "null" ]]; then
+    LOG_LEVEL="info"
+fi
 
 if [[ -z "$PANGOLIN_ENDPOINT" || -z "$NEWT_ID" || -z "$NEWT_SECRET" || "$PANGOLIN_ENDPOINT" == "null" ]]; then
     echo "❌ ERROR: Missing required configuration values!"
@@ -21,13 +25,11 @@ fi
 
 echo "✅ Configuration Loaded:"
 echo "  PANGOLIN_ENDPOINT=$PANGOLIN_ENDPOINT"
-echo "  NEWT_ID=$NEWT_ID"
-echo "  NEWT_SECRET=$NEWT_SECRET"
 
 # 🔁 Auto-reconnect loop
 while true; do
     echo "🔹 Starting Newt..."
-    PANGOLIN_ENDPOINT="$PANGOLIN_ENDPOINT" NEWT_ID="$NEWT_ID" NEWT_SECRET="$NEWT_SECRET" /usr/bin/newt
+    PANGOLIN_ENDPOINT="$PANGOLIN_ENDPOINT" NEWT_ID="$NEWT_ID" NEWT_SECRET="$NEWT_SECRET" /usr/bin/newt --log-level "$LOG_LEVEL"
 
     echo "⚠️ Newt stopped! Waiting 5 second before reconnecting..."
     sleep 5
